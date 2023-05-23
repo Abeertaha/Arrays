06c814d24279695bddb9e6629f2747a1c73e0ef0
Abeer Taha <104815752+Abeertaha@users.noreply.github.com>
2023-05-24 01:18:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] myArray = { 0, 1, 2, 3, 4 };
+        Console.WriteLine(myArray[0]);  
+        Console.WriteLine(myArray[2]);  
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine(myArray[i]);
+        }
+    }
+}
